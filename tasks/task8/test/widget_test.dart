f03d90974ebcbdee1d7e90b061e7756c2c7d40f7
semonhany <semonhany848@gmail.com>
2026-02-26 21:20:58 +0200

    organize directory structure + fix task8/test/widget_test.dart

--- tasks/task8/test/widget_test.dart
+++ tasks/task8/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tasks/main.dart';
+import 'package:task8/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
